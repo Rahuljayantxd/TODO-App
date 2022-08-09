40ba54d7af281ed49e01206c88320ff09d004caa
Rahul Kumar <444556rahul@gmail.com>
2022-08-10 01:10:00 +0530

    EJS first template

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
 const express = require("express");
 const bodyParse = require("body-parser");
 
+
 const app = express();
+
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
 
   var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-  if (today.getDay() == 6 || today.getDay() == 0) {
-    res.send("Yay it's the weekend");
-  } else {
-    res.send("Boo! I have to work!");
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+        case 3:
+        day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturaday";
+      break;
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
   }
+
+  res.render("list", {
+    kindOfDay: day
+  });
 
 });
 
