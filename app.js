b6f3ebefe8017011ae318babf8ed9cec501becc3
Rahul Kumar <444556rahul@gmail.com>
2022-08-11 15:19:44 +0530

    learnend about templating vs layout

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Eat Food", "Buy Food", "Cook Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"))
 
 app.get("/", function(req, res) {
@@ -22,15 +25,38 @@ app.get("/", function(req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
 
 });
 
 app.post("/", function(req, res) {
+
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
 });
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about")
+})
+
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
